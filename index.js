b5084b38530732f87436341440ff482ac34792c1
Alistair-Leslie-Graham <alfie.dang3r@gmail.com>
2019-05-05 21:24:57 -0700

    fix

--- index.js
+++ index.js
@@ -2,17 +2,6 @@ var express = require("express");
 var mysql = require('mysql');
 var app = express();
 
-
-if(process.env.JAWSDB_URL){
-	var connection = mysql.createConnection(process.env.JAWSDB_URL)
-} else {
-	var connection = mysql.createConnection({
-		host: 'localhost',
-		user: 'root',
-		password: 'password',
-		database: "web-db"
-	});
-}
 // set port
 var port = process.env.PORT || 8080;
 
